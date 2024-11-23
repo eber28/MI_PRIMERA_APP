8e87a13183a99dfff5a2d6c9b6b79a0455a6fc50
eber28 <eberromanpumachahua@gmail.com>
2024-11-22 22:06:00 -0500

    trabajo terminado

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,20 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:mi_primera_app/favorites_page.dart';
+import 'package:mi_primera_app/generator_page.dart';
 import 'package:mi_primera_app/providers/my_app_state.dart';
 import 'package:provider/provider.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    var appstate = context.watch<MyAppState>();
-    return Scaffold(
-      body: Column(
-        children: [
-          const Text("A ramdow idea:"),
-          Text(appstate.current.asLowerCase),
-        ],
-      ),
-    );
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = FavoritesPage();
+      default:
+        throw UnimplementedError('No Widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            Stack(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Inicio'),
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.favorite),
+                        label: Text('Favoritos'),
+                      ),
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: constraints.maxWidth >= 600
+                        ? Text(
+                            '© ${DateTime.now().year} Narmeshit',
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          )
+                        : Text(
+                            '© Nms',
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+                child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            )),
+          ],
+        ),
+      );
+    });
   }
 }
--- lib/providers/my_app_state.dart
+++ lib/providers/my_app_state.dart
@@ -3,4 +3,29 @@ import 'package:flutter/material.dart';
 
 class MyAppState with ChangeNotifier { 
   var current = WordPair.random();
+  var history = <WordPair>[];
+  GlobalKey? historyListKey;
+
+  void getNext() {
+    history.insert(0, current);
+    var animatedList = historyListKey?.currentState as AnimatedListState;
+    animatedList.insertItem(0);
+    current = WordPair.random();
+    notifyListeners();
+  }
+  var favorites = <WordPair>[];
+
+  void toogleFavorite(WordPair? pair){
+    pair = pair ?? current;
+    if(favorites.contains(pair)){
+      favorites.remove(pair);
+    }else {
+      favorites.add(pair);
+    }
+    notifyListeners();
+    }
+    void removeFavorite(WordPair? pair){
+    favorites.remove(pair);
+    notifyListeners();
+  }
 }
